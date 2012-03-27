1a52022d9440a110ae9d3eacda1719367ea0fbeb
Dandre Allison <dandre.allison@gmail.com>
2012-03-26 20:49:54 -0700

    Add SendGrid smtp and bettafish favicon

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,19 +1,27 @@
 require 'development_mail_interceptor'
 
-ActionMailer::Base.smtp_settings = {
-	:address              => "smtp.gmail.com",
-	:port                 => 587,
-	:domain               => "betatrek.com",
-	:user_name            => "emailconfirmation@betatrek.com",
-	:password             => "thisisabettafish",
-	:authentication       => "plain",
-	:enable_starttls_auto => true
-}
-
-# Sets the default host to use to create urls in the ActionMailer based on the environment
 if Rails.env.production?
+	ActionMailer::Base.smtp_settings = {
+		:address              => "smtp.sendgrid.com",
+		:port                 => 587,
+		:domain               => "betatrek.com",
+		:user_name            => "bettafish",
+		:password             => "deliverybettafish",
+		:authentication       => "plain",
+		:enable_starttls_auto => true
+	}
     ActionMailer::Base.default_url_options[:host] = "ec2-23-20-185-26.compute-1.amazonaws.com"# temporary "betatrek.com"
 else
+	ActionMailer::Base.smtp_settings = {
+		:address              => "smtp.gmail.com",
+		:port                 => 587,
+		:domain               => "betatrek.com",
+		:user_name            => "emailconfirmation@betatrek.com",
+		:password             => "thisisabettafish",
+		:authentication       => "plain",
+		:enable_starttls_auto => true
+	}
     ActionMailer::Base.default_url_options[:host] = "localhost:3000"
 end
+
 ActionMailer::Base.register_interceptor DevelopmentMailInterceptor if Rails.env.development?
