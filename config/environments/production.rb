7520bb7ea7c1e85a161e4bffe508352d3c13f1ce
Dandre Allison <dandre.allison@gmail.com>
2012-03-31 09:28:24 -0700

    Change to memory_store

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,7 @@ BetatrekOnRails::Application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
 
   # Use a different cache store in production
-  config.cache_store = :mem_cache_store, "107.20.190.97:11211"
+  config.cache_store = :memory_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
