b599b4ae22ddf32c925882e7a83e53bf2bbce21c
Dandre Allison <dandre.allison@gmail.com>
2012-04-02 21:33:51 -0700

    Add new RSVP page cache and comments throughout

--- app/mailers/rsvp_mailer.rb
+++ app/mailers/rsvp_mailer.rb
@@ -1,6 +1,7 @@
 class RsvpMailer < ActionMailer::Base
   default from: "emailconfirmation@betatrek.com"
 
+  # Performed before sending the RSVP email confirmation
   def email_confirmation rsvp
   	@rsvp = rsvp
     mail to: rsvp.email, subject: "Thank you for RSVPing"
--- app/models/rsvp.rb
+++ app/models/rsvp.rb
@@ -14,6 +14,7 @@
 class Rsvp < ActiveRecord::Base
 	attr_accessible :email
 
+	# Assigns a unique ID and confirmation code to a new Rsvp
 	before_save :create_uid
 	before_save :create_confirmation_code
 
@@ -23,14 +24,18 @@ class Rsvp < ActiveRecord::Base
 					  format: { with: VALID_EMAIL_REGEX },
 					  uniqueness: { case_sensitive: false }
 
+	# Uses the UID as the URI for Rsvps
 	def to_param
 		uid
 	end
 
+	# Query if the Rsvp has been confirmed
 	def confirmed?
 		confirmed
 	end
 
+	# Checks the confirmation code sent against the stored code and update the Rsvp as
+	# being confirmed if they match
 	def confirm_email confirmation_code
 		if (not confirmation_code.nil?) && confirmation_code == self.confirmation_code
 			self.confirmed = true
@@ -41,10 +46,12 @@ class Rsvp < ActiveRecord::Base
 
 	private
 
+		# Creates a secure random UID for the RSVP if it doesn't already have one
 		def create_uid
 			self.uid ||= SecureRandom.urlsafe_base64
 		end
 
+		# Creates a random confirmation code for the RSVP unless it is already confirmed
 		def create_confirmation_code
 			self.confirmation_code = Digest::SHA1.hexdigest("salt #{Time.now.to_f}") unless confirmed?
 		end
